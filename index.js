a0d06bf898acf1f8c40b2d7900ba8d12a02c74d1
NGLYRY <130323105+NGLYRY@users.noreply.github.com>
2025-03-02 14:35:58 -0800

    Set up rows and columns in console for vital db

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
-d3.csv("https://api.vitaldb.net/cases").then(function(data) {
-  console.log(data);
+const data = d3.csv("https://api.vitaldb.net/cases").then(function(data) {
+    console.log(data);
+    const columns = data.columns;
+    const rows = data.map(row => Object.values(row));
+
+    console.log("Columns:", columns);
+    console.log("Rows:", rows);
 });
+
