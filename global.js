c07af1de6cc20420be9f6c9571505adaa6b13e99
NGLYRY <130323105+NGLYRY@users.noreply.github.com>
2025-03-10 15:44:30 -0700

    fixing nav again

--- global.js
+++ global.js
@@ -1,21 +1,20 @@
 console.log("global.js loaded");
 
 let pages = [
-    { url: '', title: 'Survey'},
-    { url: 'writeup/writeup.html', title: 'Writeup'}
+    { url: 'index.html', title: 'Survey'},
+    { url: 'writeup/', title: 'Writeup'}
   ];
   
   let nav = document.createElement('nav');
   document.body.prepend(nav);
 
-  const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
   for (let p of pages) {
     let url = p.url;
     let title = p.title;
     
-    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
-    
+    url = `./${url}`;
+
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
